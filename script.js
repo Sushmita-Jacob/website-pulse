e566ed03775dfdca797c188dbd9b0e4e32a01910
Sushmita-Jacob <sushmita.cjacob@gmail.com>
2026-08-24 06:15:24 -0400

    added data type conditional and error handling

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
+let media;
 const API_KEY = import.meta.env.VITE_NASA_API_KEY;
-document.querySelector("#app").innerHTML = "<p>loading...</p>";
+document.querySelector("#app").innerHTML = "<p>:Loading...</p>";
 const date = document.querySelector("#datepicker").value;
-fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&date=${date}`).then(response => response.json()).then(data => {
+fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&date=${date}`)
+.then(response => response.json())
+.then(data => {
     console.log(data);
-    document.querySelector("#app").innerHTML = ` <h1>${data.title}</h1> `;
+    if (data.media_type === "image") {
+        // Show image
+        media = `<img src="${data.url}"/>`;
+    } else if (data.media_type === "video") {
+        // Show video
+        media = `<video src="${data.url}" controls></video>`;
+    } else {
+        // Show YouTube
+        media = `<iframe src="${data.url}"/></iframe>`
+    }
+    document.querySelector("#app").innerHTML = `
+    <h1>${data.title}</h1>
+    ${media}
+    <p>${data.explanation}</p> `;
 })
+    .catch(err => {
+        document.querySelector("#app").innerHTML = `<p>Error: ${err.message}</p>`;
+    })
